d1557b31e90695cf4518137e6078327079eea1d0
SixHourDays <jeffgiffen@gmail.com>
2016-05-06 13:57:31 -0400

    Ship up
    + add ship prefab
    + ship / planet collision on

---
+++ WrathOfKhan/Assets/PlayerShipScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShipScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = transform.position + new Vector3(0.01f,0.0f,0.0f);
+
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        GameObject.Destroy(gameObject); //this context is the script comopnent. destroy your master too, not just you!!
+    }
+}
