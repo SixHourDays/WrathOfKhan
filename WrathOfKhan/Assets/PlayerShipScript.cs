408a35a20be46e24de6bf597085391b7601f5da3
SixHourDays <jeffgiffen@gmail.com>
2016-05-06 14:24:56 -0400

    Physics!
    Ship is live, doing n body gravity.  Get a nice figure 8 turn out of it.

--- WrathOfKhan/Assets/PlayerShipScript.cs
+++ WrathOfKhan/Assets/PlayerShipScript.cs
@@ -12,8 +12,33 @@ public class PlayerShipScript : MonoBehaviour {
 	void Update () {
 
         transform.position = transform.position + new Vector3(0.01f,0.0f,0.0f);
-
 	}
+
+    Vector3 GetNBodyForceAtPos(Vector3 pos )
+    {
+        GameObject[] gravBodies = GameObject.FindGameObjectsWithTag("GravBody");
+
+        Vector3 gravForce = new Vector3(0, 0, 0);
+        foreach(GameObject go in gravBodies)
+        {
+            // f = GM / r2 when orbiting mass is insignificant
+            Vector3 offset = go.transform.position - pos;
+            float forceMag = go.GetComponent<GravBodyScript>().GM / offset.sqrMagnitude;
+            Vector3 force = offset.normalized * forceMag;
+            gravForce += force;
+        }
+        return gravForce;
+    }
+
+    public float mass {  get { return GetComponent<Rigidbody2D>().mass; } }
+    public Vector3 velocity;
+
+    void FixedUpdate()
+    {
+        Vector3 gravForce = GetNBodyForceAtPos(transform.position);
+        velocity += gravForce * Time.fixedDeltaTime;
+        transform.position += velocity * Time.fixedDeltaTime;
+    }
 
     void OnCollisionEnter2D(Collision2D col)
     {
