ebba8aaa71b89bf4b73dcee9c87050783453ec37
SixHourDays <jeffgiffen@gmail.com>
2016-05-06 13:19:23 -0400

    Planets up
    +planet prefab
    + two temp textures
    * camera to ortho & -1z

---
+++ WrathOfKhan/Assets/GravBodyScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravBodyScript : MonoBehaviour {
+
+    public float GM;
+    public float radius { get { return GetComponent<CircleCollider2D>().radius; } }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
