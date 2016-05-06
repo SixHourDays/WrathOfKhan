395709e096c582690a2a6f8b0fd942b9d718b51f
SixHourDays <jeffgiffen@gmail.com>
2016-05-06 11:27:58 -0400

    The Fabled Origins
    + design ref materials
    + primary loader scene to control all else
    + empty gameplay scene

---
+++ WrathOfKhan/Assets/LoaderScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoaderScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        //TODOJEFFGIFFEN write scene manager loader code here
+	}
+}
