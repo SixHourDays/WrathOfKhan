4edafea8d389522df24c55e2934aa678dc189aec
SixHourDays <jeffgiffen@gmail.com>
2016-05-06 19:19:26 -0400

    Aimer + Torpedo Shooting
    n step physics based aimer,
    and torpedo shooting

--- WrathOfKhan/Assets/Scenes/Gameplay/Ship/PlayerShipScript.cs
+++ WrathOfKhan/Assets/Scenes/Gameplay/Ship/PlayerShipScript.cs
@@ -5,24 +5,75 @@ public class PlayerShipScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 
+    public GameObject camera;
     public GameObject torpedoGO;
+    public GameObject aimerDotGO;
+    public int aimerDotCount;
+    public float torpedoVelo;
+
+    //true - movement. false - pause + aimer
+    bool flying = true;
 
 	// Update is called once per frame
 	void Update () {
 
-        if ( Input.anyKeyDown )
+        if (Input.GetKeyDown(KeyCode.G))
         {
-            Debug.Log(transform.forward * 10.0f);
-            GameObject torp = (GameObject)Instantiate(torpedoGO, transform.position + velocity * 0.2f, new Quaternion());
-            torp.GetComponent<TorpedoScript>().velocity = velocity;
+            flying = !flying;
+            for (int i = 0; i < transform.childCount; ++i) { transform.GetChild(i).gameObject.SetActive(!flying); }
+        }
+        if ( !flying )
+        {
+            //keeps dot count matched to public int
+            int toSpawn = aimerDotCount - transform.childCount;
+            if (toSpawn > 0)
+            {
+                for (int i = 0; i < toSpawn; ++i)
+                {
+                    GameObject dotChild = Instantiate(aimerDotGO);
+                    dotChild.transform.parent = transform;
+                    dotChild.SetActive(true);
+                }
+            }
+            else if (toSpawn < 0)
+            {
+                for (int i = transform.childCount - 1; i >= aimerDotCount; --i) { GameObject.Destroy(transform.GetChild(i)); }
+            }
+
+            //dot aimer
+            Vector3 aimerPos = camera.GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
+            aimerPos.z = 0;
+            Vector3 aimerDir = aimerPos.normalized;
+            Vector3 aimerStart = transform.position + aimerDir * (GetComponent<CircleCollider2D>().radius + 0.1f); //step outside
+
+            //iterate n fixed steps
+            Vector3 stepPos = aimerStart;
+            Vector3 stepVelo = aimerDir * torpedoVelo;
+            for (int i = 0; i < aimerDotCount; ++i)
+            {
+                Vector3 gravForce = TorpedoScript.GetNBodyForceAtPos(stepPos);
+                Vector3 accel = gravForce / mass;
+                stepVelo += accel * Time.fixedDeltaTime;
+                stepPos += stepVelo * Time.fixedDeltaTime;
+                transform.GetChild(i).transform.position = stepPos;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameObject torp = (GameObject)Instantiate(torpedoGO, aimerStart, new Quaternion());
+                torp.GetComponent<TorpedoScript>().velocity = aimerDir * torpedoVelo;
+                flying = true;
+                for (int i = 0; i < transform.childCount; ++i) { transform.GetChild(i).gameObject.SetActive(false); }
+            }
         }
 	}
 
     void FixedUpdate()
     {
+        if (!flying) { return; }
         Vector3 gravForce = TorpedoScript.GetNBodyForceAtPos(transform.position);
         Vector3 accel = gravForce / mass;
         velocity += accel * Time.fixedDeltaTime;
