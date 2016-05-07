f1c6fb962f474f37a6fc97e5ea9fdb21faf0538b
BladeBreaker <daniel.liverance@gmail.com>
2016-05-07 15:37:25 -0400

    Forgot the new comment

--- WrathOfKhan/Assets/Networking/TransmissionObjects/TransmissionInfo.cs
+++ WrathOfKhan/Assets/Networking/TransmissionObjects/TransmissionInfo.cs
@@ -8,6 +8,7 @@ public class TransmissionInfo
     // name that corresponds to the static Name field of each Transmission
     public string transmission_name;
 
+    // playerID that the transmission originated from. Used to tell if we should propegate or not.
     public int transmission_from_id;
 
     // payload, or json, to deserialize into the Transmission (once we know the specific type)
