eaf1e36267abd812c61d53432ab01ccaa5653e05
BladeBreaker <daniel.liverance@gmail.com>
2016-05-08 14:03:23 -0400

    SHIELDS FIXED

--- WrathOfKhan/Assets/Scenes/Gameplay/Ship/PlayerShipScript.cs
+++ WrathOfKhan/Assets/Scenes/Gameplay/Ship/PlayerShipScript.cs
@@ -203,7 +203,7 @@ public class PlayerShipScript : MonoBehaviour
 
     PlayerTurnSteps ChooseOrDone()
     {
-        if (m_shipState.torpedosRemaining > 0 || m_shipState.shieldsRemaining > 0 || m_shipState.enginesRemaining > 0)
+        if (m_shipState.torpedosRemaining > 0 || m_shipState.enginesRemaining > 0)
         {
             Debug.Log("Start ChooseAction");
             UIManager.Get().SetPhaseTwoActive(); //sets all buttons on
