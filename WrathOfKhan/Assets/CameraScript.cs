b11b6f9491fc4533e34e0bc65be387979001d260
SixHourDays <jeffgiffen@gmail.com>
2016-05-08 11:28:51 -0400

    scale players, make cam follow player

---
+++ WrathOfKhan/Assets/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    
+	void Update () {
+
+        Vector3 playerPos = GameplayScript.Get().GetLocalPlayer().transform.position;
+        transform.position = new Vector3(playerPos.x, playerPos.y, -1);
+	}
+}
