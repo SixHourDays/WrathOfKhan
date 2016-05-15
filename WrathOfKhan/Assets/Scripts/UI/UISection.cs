e999a47b498dfc749cd738e053b4347f9faa5eb2
SixHourDays <jeffgiffen@gmail.com>
2016-05-14 22:16:48 -0400

    fix a nagging null ref

--- WrathOfKhan/Assets/Scripts/UI/UISection.cs
+++ WrathOfKhan/Assets/Scripts/UI/UISection.cs
@@ -7,15 +7,20 @@ public class UISection : MonoBehaviour
     private GameObject m_darken;
     private RectTransform[] m_controls;
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    private bool initialized = false;
+    void Start()
     {
+        if (initialized) { return; }
+
         m_darken = this.transform.FindChild("Darken").gameObject;
         Debug.Assert(m_darken != null);
 
         GameObject ctrlObj = this.transform.FindChild("Controls").gameObject;
         Debug.Assert(ctrlObj != null);
         m_controls = ctrlObj.transform.GetComponentsInChildren<RectTransform>();
+
+        initialized = true;
 	}
 	
 	// Update is called once per frame
@@ -38,7 +43,9 @@ public class UISection : MonoBehaviour
 
     public void SetActive(bool active)
     {
-        if( active )
+        if (!initialized) { Start(); }
+
+        if ( active )
         {
             m_darken.SetActive(false);
 
